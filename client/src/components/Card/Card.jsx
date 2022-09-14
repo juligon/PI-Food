15f7eee3d18fc183373df23b78a9026a8f18be39
Juliana <julianagon73@gmail.com>
2022-09-14 12:55:22 -0300

    update

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import style from "./Card.module.css";
 
 export default function Card({ title, diets, image, id }) {
 	return (
-		<div className={style.card}>
+		<div className={style.card} key={id}>
 			<h4 className={style.title}>{title}</h4>
 			{diets?.map((e, index) => (
 				<p key={index} className={style.diets}>
